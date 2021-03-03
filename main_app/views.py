5959d2774d59c0f561487bb62de17fc08a837e6c
eshoe2020 <hsulainey@gmail.com>
2021-03-02 19:55:38 -0700

    adds authorization to comment and article views, user must now be logged in to access functionalities

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,8 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .forms import CommentForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
   return render(request,'home.html')
@@ -12,16 +13,18 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def articles_index(request):
-  articles = Article.objects.all()
+  articles = request.user.article_set.all()
   return render(request, 'articles/index.html', { 'articles': articles })
 
+@login_required
 def articles_detail(request, article_id):
   article = Article.objects.get(id=article_id)
   comment_form = CommentForm()
   return render(request, 'articles/detail.html', {'article': article, 'comment_form': comment_form})
 
-
+@login_required
 def add_comment(request, article_id):
   form = CommentForm(request.POST)
   if form.is_valid():
@@ -30,16 +33,15 @@ def add_comment(request, article_id):
     new_comment.save()
   return redirect('articles_detail', article_id = article_id)
 
-
-def delete_comment(request, article_id):
-  article = Article.objects.get(id=article_id)
+@login_required
+def delete_comment(request, comment_id):
+  # article = Article.objects.get(id=article_id)
   # form = CommentForm(request.POST)
-  Article.objects.get(pk=article_id).delete()
-
+  Comment.objects.get(comment_id).delete()
   return redirect('articles_detail', article_id = article_id)
 
 
-class CreateArticle(CreateView):
+class CreateArticle(LoginRequiredMixin, CreateView):
   model = Article
   fields = '__all__'
 
@@ -48,13 +50,14 @@ class CreateArticle(CreateView):
     return super().form_valid(form)
 
 
-class ArticleUpdate(UpdateView):
+class ArticleUpdate(LoginRequiredMixin, UpdateView):
   model = Article
   fields = ['content']
 
-class ArticleDelete(DeleteView):
+class ArticleDelete(LoginRequiredMixin, DeleteView):
   model = Article
   success_url='/articles/'
+
 
 def signup(request):
   error_message = ''
